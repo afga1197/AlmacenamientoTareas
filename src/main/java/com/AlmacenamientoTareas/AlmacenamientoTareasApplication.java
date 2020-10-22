d5a1f5b6e30e92e751325e5719678156b1e3f4f7
andres gomez <andrefelgoal@gmail.com>
2020-10-22 09:44:00 -0500

    Back End prueba Lumon

---
+++ src/main/java/com/AlmacenamientoTareas/AlmacenamientoTareasApplication.java
@@ -0,0 +1,13 @@
+package com.AlmacenamientoTareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmacenamientoTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmacenamientoTareasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AlmacenamientoTareas/AlmacenamientoTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AlmacenamientoTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmacenamientoTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
